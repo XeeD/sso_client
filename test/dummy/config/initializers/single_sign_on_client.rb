f749f696cd177e5697603d4e60f721341f048a47
XeeD <xeedus@gmail.com>
2012-12-29 20:10:29 +0100

    Move content of configuration options to YAML file in test dummy
    It will be easier to install and configure this gem for development

--- test/dummy/config/initializers/single_sign_on_client.rb
+++ test/dummy/config/initializers/single_sign_on_client.rb
@@ -1,6 +1,19 @@
+SSO_PROVIDER_SETTINGS_FILE = File.join(Rails.root, "config", "sso_provider.yml")
+
+unless File.exist?(SSO_PROVIDER_SETTINGS_FILE)
+  raise <<-CONFIG_ERROR
+You need to create #{SSO_PROVIDER_SETTINGS_FILE} file in config/ directory.
+Example can be found in config/sso_provider.sample.yml file
+CONFIG_ERROR
+end
+
+# Load settings
+SSO_PROVIDER = YAML.load_file(SSO_PROVIDER_SETTINGS_FILE).symbolize_keys
+
+# Setup the client
 SingleSignOnClient.setup do |config|
-  config.provider_url = "http://localhost:3000"
-  config.app_id = "ELVO"
-  config.app_secret = "1234"
+  config.provider_url   = SSO_PROVIDER[:provider_url]
+  config.app_id         = SSO_PROVIDER[:app_id]
+  config.app_secret     = SSO_PROVIDER[:app_secret]
   config.user_decorator = UserDecorator
 end
